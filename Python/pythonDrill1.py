058d554ac597dfc42e1542ce382c157cb44fc987
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-04-10 13:32:15 -0500

    Edit pythonDrill Add pythonAssignment 1 and 2

--- Python/pythonDrill1.py
+++ Python/pythonDrill1.py
@@ -1,4 +1,4 @@
-@@ -0,0 +1,21 @@
+
 
 import os
 
@@ -8,16 +8,19 @@ path2 = "Python\\" # second part of the path
 
 path = os.path.join(path1, path2) # concatenating the two parts of the path
 
-print(os.listdir(path)) # printing all files and folders in the Python folder
-                        # in the project folder on the desktop
+for file in os.listdir(path):
+    if file.endswith(".txt"):
+        print (os.path.join(path, file))
+
+ 
 
 
-mTime = os.path.getmtime("C:\\python_projects\\pythonAssignment.txt")
+mTime = os.path.getmtime("C:\\Users\\Schui\\Desktop\\project\\Python\\pythonAssignment.txt")
 fText1 = ("pythonAssignment.txt")
 print("{} has been created {} seconds ago".format(fText1, mTime))
 
 
-mTime1 = os.path.getmtime("C:\\python_projects\\pythonAssignment2.txt")
+mTime1 = os.path.getmtime("C:\\Users\\Schui\\Desktop\\project\\Python\\pythonAssignment2.txt")
 fText2 = ("pythonAssignment2.txt")
 print("{} has been created {} seconds ago".format(fText2, mTime1))
 
